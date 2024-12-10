699c197847d2339ed4dc44f7164d5cbe1e8bef29
Dennshirennshij <111683708+Dennshirennshij@users.noreply.github.com>
2024-12-10 13:45:05 +0100

    Basically finished the project

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Development environment for raylib on NixOS with wayland";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            openssl
+            wget
+          ];
+
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          LD_LIBARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl ];
+
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          env = {
+            OPENSSL_LIB = "${pkgs.openssl.dev}/";
+          };
+
+          shellHook = ''
+          '';
+        };
+      }
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,135 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser};
+use serde_json::Value;
+use std::{error::Error, process::Command, path::{Path, PathBuf}, str::FromStr };
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    pack_id: u32,
+    #[arg(short, long)]
+    release: u32,
+    #[arg(short, long)]
+    out: PathBuf,
+    #[arg(short, long, default_value = "true")]
+    client: bool,
+}
+
+fn parse_targets(targets: serde_json::Value) -> Result<(), Box<dyn Error>> {
+    let mut i = 0;
+    loop {
+        let current_target = targets[i].clone();
+        if current_target["id"].as_i64().is_none() {
+            break;
+        }
+
+        let id = current_target["id"].as_i64().unwrap();
+        let name = current_target["name"].as_str().unwrap();
+        let version = current_target["version"].as_str().unwrap();
+        let r#type = current_target["type"].as_str().unwrap();
+        let updated = current_target["updated"].as_u64().unwrap();
+
+        println!(
+            "i: {}\nid: {}\nname: {}\nversion: {}\ntype: {}\nupdated: {}\n\n\n",
+            i, id, name, version, r#type, updated
+        );
+
+        i += 1;
+    }
+    Ok(())
+}
+
+#[derive(Debug, Hash, Eq, PartialEq, PartialOrd, Ord)]
+struct OnlineFile {
+    path: PathBuf,
+    url: String,
+    server: bool,
+    client: bool,
+}
+impl OnlineFile {
+    fn from_json(json: serde_json::Value) -> Result<Self, Box<dyn Error>> {
+        if json["clientonly"].as_bool().is_none() {
+            return Err("Error".into());
+        }
+        let client_only = json["clientonly"].as_bool().unwrap();
+        let server_only = json["serveronly"].as_bool().unwrap();
+        println!("Parsing File: {:?}", json);
+        let mut path = PathBuf::from_str(json["path"].as_str().unwrap())?;
+        path.push(json["name"].as_str().unwrap());
+
+        let url = match json["mirrors"][0].as_str() {
+            Some(url) => url,
+            None => json["url"].as_str().unwrap(),
+        }.to_string();
+
+        Ok(OnlineFile {
+            path,
+            url,
+            server: !client_only,
+            client: !server_only,
+        })
+    }
+    fn parse_files(json: serde_json::Value) -> Result<Vec<OnlineFile>, Box<dyn Error>> {
+        let mut result: Vec<OnlineFile> = Vec::new();
+        let mut i = 0;
+        loop {
+            println!("i: {}", i);
+            if let Ok(file) = OnlineFile::from_json(json[i].clone()) {
+                result.push(file);
+            } else {
+                break;
+            }
+            i += 1;
+            println!("");
+        }
+        Ok(result)
+    }
+    fn download(&self, output_dir: &Path) -> Result<(), Box<dyn Error>> {
+        println!("Downloading {:?}", self.path);
+        let mut path: PathBuf = output_dir.to_path_buf();
+        path.push(self.path.clone());
+        
+        let _ = Command::new("curl")
+            .arg(self.url.trim())
+            .arg("--output")
+            .arg(path)
+            .arg("--create-dirs").spawn()?;
+
+
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    println!("{:?}", args);
+
+    let mut res = reqwest::get(format!(
+        "https://api.feed-the-beast.com/v1/modpacks/public/modpack/{}/{}/",
+        args.pack_id, args.release
+    ))
+    .await?
+    .text()
+    .await?;
+
+    let json: Value = serde_json::from_str(&res)?;
+
+    let targets = json["targets"].clone();
+    println!("{:?}", &targets);
+
+    parse_targets(targets)?;
+    let files = OnlineFile::parse_files(json["files"].clone())?;
+    println!("1: {:?}\n2: {:?}\n3: {:?}", files.get(0), files.get(1), files.get(2));
+
+    for file in files.iter().filter(|file| {
+        if args.client {
+            file.client
+        } else {
+            file.server
+        }
+    }) {
+        file.download(&args.out)?;
+    }
+
+    Ok(())
 }
