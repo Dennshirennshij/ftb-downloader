769f5ca82436e9d3b47efc4a4deace66c272c645
Dennshirennshij <111683708+Dennshirennshij@users.noreply.github.com>
2024-12-25 17:23:16 +0100

    Made the GUI work

--- src/gui.rs
+++ src/gui.rs
@@ -1,52 +1,142 @@
+use crate::downloader::{download, ProgressUpdate};
+use eframe::egui::{CentralPanel, ViewportBuilder};
+use egui::{Button, ProgressBar};
 use std::path::PathBuf;
-use eframe::egui::{CentralPanel, Context, TopBottomPanel, ViewportBuilder};
+use std::str::FromStr;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use tokio::runtime::Runtime;
 
-struct TestApp {
-    name: String,
-    age: u32,
+struct InputApp {
+    pack_id: String,
+    release_id: String,
+    path: PathBuf,
+    updates: Option<Arc<Mutex<ProgressUpdate>>>,
 }
-impl Default for TestApp {
+impl Default for InputApp {
     fn default() -> Self {
         Self {
-            name: "Max Mustermann".to_string(),
-            age: 42,
+            pack_id: "0".to_string(),
+            release_id: "0".to_string(),
+            path: PathBuf::from_str("./output").unwrap(),
+            updates: None,
         }
     }
 }
+impl eframe::App for InputApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let download_server = Button::new("Download server files");
+        let download_client = Button::new("Download client files");
 
-impl eframe::App for TestApp {
-    fn update(&mut self, ctx: &Context, frame: &mut eframe::Frame) {
+        // Remove the value from the option when the download is completed
+        if let Some(update) = &self.updates {
+            let clone = update.clone();
+            let lock = clone.lock().unwrap();
+            if lock.done {
+                self.updates = None;
+            }
+            drop(lock);
+        }
         CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Give me your data now!");
-
+            ui.heading("Download a FTB Modpack without the stupid app");
             ui.horizontal(|ui| {
-                ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name);
+                ui.label("Pack ID: ");
+                if self.updates.is_some() {
+                    ui.label(&self.pack_id);
+                } else {
+                    ui.text_edit_singleline(&mut self.pack_id);
+                }
             });
-
+            if self.pack_id.parse::<u32>().is_err() {
+                ui.label("Pack ID is not a number!!!");
+            }
             ui.separator();
+            ui.horizontal(|ui| {
+                ui.label("Release ID: ");
+                if self.updates.is_some() {
+                    ui.label(&self.release_id);
+                } else {
+                    ui.text_edit_singleline(&mut self.release_id);
+                }
+            });
+            if self.release_id.parse::<u32>().is_err() {
+                ui.label("Release ID is not a number!!!");
+            }
+            ui.separator();
+            if let Some(update) = &self.updates {
+                let lock = update.lock().unwrap();
+                
+                let progress = lock.downloaded_files as f32 / lock.total_files as f32;
+                ui.add(ProgressBar::new(progress));
+                
+                drop(lock);
+            } else {
+                ui.horizontal(|ui| {
+                    if ui.add(download_server).clicked()
+                        && !(self.pack_id.parse::<u32>().is_err()
+                            || self.release_id.parse::<u32>().is_err())
+                    {
+                        let pack = self.pack_id.parse::<u32>().unwrap();
+                        let release = self.release_id.parse::<u32>().unwrap();
 
-            ui.label(format!("Your name is {}.", &self.name));
+                        let progress = Arc::new(Mutex::new(ProgressUpdate::default()));
+                        self.updates = Some(progress.clone());
 
+                        spawn_thread(pack, release, false, self.path.clone(), progress);
+                    }
+                    if ui.add(download_client).clicked()
+                        && !(self.pack_id.parse::<u32>().is_err()
+                            || self.release_id.parse::<u32>().is_err())
+                    {
+                        let pack = self.pack_id.parse::<u32>().unwrap();
+                        let release = self.pack_id.parse::<u32>().unwrap();
+
+                        let progress = Arc::new(Mutex::new(ProgressUpdate::default()));
+                        self.updates = Some(progress.clone());
+
+                        spawn_thread(pack, release, true, self.path.clone(), progress);
+                    }
+                });
+            }
         });
     }
 }
 
-pub fn open_test_gui() -> eframe::Result {
-    let native_options = eframe::NativeOptions {
-        viewport: ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0]),
-        ..Default::default()
-    };
-
-    eframe::run_native(
-        "Test application",
-        native_options,
-        Box::new(|_cc| Ok(Box::new(TestApp::default()))),
-    )
+fn spawn_thread(
+    pack: u32,
+    release: u32,
+    client: bool,
+    output: PathBuf,
+    progress: Arc<Mutex<ProgressUpdate>>,
+) {
+    let _handle = thread::spawn(move || {
+        println!("Spawned thread");
+        let result = Runtime::new().unwrap().block_on(download(
+            pack,
+            release,
+            client,
+            output,
+            Some(progress),
+        ));
+        match result {
+            Ok(()) => println!("No errors"),
+            Err(e) => println!("Error: {:?}", e),
+        }
+        println!("Closed thread");
+    });
 }
 
-pub fn open_gui() -> (u32, u32, bool, PathBuf) {
-    todo!()
+pub fn gui_main() {
+    let options = eframe::NativeOptions {
+        viewport: ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([200.0, 150.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "FTB Downloader",
+        options,
+        Box::new(|_cc| Ok(Box::new(InputApp::default()))),
+    )
+    .expect("Failed to create the GUI");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
+mod downloader;
 mod gui;
 
 use clap::Parser;
 use core::panic;
-use serde_json::Value;
-use std::{
-    error::Error,
-    fmt::Debug,
-    path::{Path, PathBuf},
-    process::Command,
-    str::FromStr,
-};
+use std::{error::Error, fmt::Debug, path::PathBuf};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -25,140 +19,36 @@ struct Args {
     cli: bool,
 }
 
-fn parse_targets(targets: serde_json::Value) {
-    let mut i = 0;
-    loop {
-        let current_target = targets[i].clone();
-        if current_target["id"].as_i64().is_none() {
-            break;
-        }
-
-        let id = current_target["id"].as_i64().unwrap();
-        let name = current_target["name"].as_str().unwrap();
-        let version = current_target["version"].as_str().unwrap();
-        let r#type = current_target["type"].as_str().unwrap();
-        let updated = current_target["updated"].as_u64().unwrap();
-
-        println!(
-            "i: {}\nid: {}\nname: {}\nversion: {}\ntype: {}\nupdated: {}\n\n\n",
-            i, id, name, version, r#type, updated
-        );
-
-        i += 1;
-    }
-}
-
-#[derive(Debug, Hash, Eq, PartialEq, PartialOrd, Ord)]
-struct OnlineFile {
-    path: PathBuf,
-    url: String,
-    server: bool,
-    client: bool,
-}
-impl OnlineFile {
-    fn from_json(json: serde_json::Value) -> Result<Self, Box<dyn Error>> {
-        if json["clientonly"].as_bool().is_none() {
-            return Err("Error".into());
-        }
-        let client_only = json["clientonly"].as_bool().unwrap();
-        let server_only = json["serveronly"].as_bool().unwrap();
-        println!("Parsing File: {:?}", json);
-        let mut path = PathBuf::from_str(json["path"].as_str().unwrap())?;
-        path.push(json["name"].as_str().unwrap());
-
-        let url = match json["mirrors"][0].as_str() {
-            Some(url) => url,
-            None => json["url"].as_str().unwrap(),
-        }
-        .to_string();
-
-        Ok(OnlineFile {
-            path,
-            url,
-            server: !client_only,
-            client: !server_only,
-        })
-    }
-    fn parse_files(json: serde_json::Value) -> Vec<OnlineFile> {
-        let mut result: Vec<OnlineFile> = Vec::new();
-        let mut i = 0;
-        loop {
-            println!("i: {}", i);
-            if let Ok(file) = OnlineFile::from_json(json[i].clone()) {
-                result.push(file);
-            } else {
-                break;
-            }
-            i += 1;
-            println!();
-        }
-        result
-    }
-    fn download(&self, output_dir: &Path) -> Result<(), Box<dyn Error>> {
-        println!("Downloading {:?}", self.path);
-        let mut path: PathBuf = output_dir.to_path_buf();
-        path.push(self.path.clone());
-
-        let _ = Command::new("curl")
-            .arg("--silent")
-            .arg(self.url.trim())
-            .arg("--output")
-            .arg(path)
-            .arg("--create-dirs")
-            .spawn()?;
-
-        Ok(())
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Open test UI
-    let _ = gui::open_test_gui();
-
     // Parse the arguments
     // or open the gui
     let args = Args::parse();
-    let (pack_id, release_id, client, out) = match args.cli {
-        true => {
-            match match (args.pack_id, args.release, args.client, args.out) {
-                (None, _, _, _) => Err("Pack ID"),
-                (_, None, _, _) => Err("Release ID"),
-                (_, _, None, _) => Err("Client"),
-                (_, _, _, None) => Err("Output"),
-                (Some(pack), Some(release), Some(cl), Some(output)) => {
-                    Ok((pack, release, cl, output))
-                }
-            } {
-                Ok(arguments) => arguments,
-                Err(var) => panic!("Variable not found: {}", var),
-            }
+
+    if args.cli {
+        if args.client.is_none() {
+            panic!("Client?");
         }
-        false => gui::open_gui(),
-    };
-
-    // Get the modpack index
-    let res = reqwest::get(format!(
-        "https://api.feed-the-beast.com/v1/modpacks/public/modpack/{}/{}/",
-        pack_id, release_id
-    ))
-    .await?
-    .text()
-    .await?;
-
-    // Parse the index of the given modpack into a struct
-    let json: Value = serde_json::from_str(&res)?;
-    let files = OnlineFile::parse_files(json["files"].clone());
-    for file in files
-        .iter()
-        .filter(|file| if client { file.client } else { file.server })
-    {
-        file.download(&out)?;
+        if args.out.is_none() {
+            panic!("Output dir?");
+        }
+        if args.pack_id.is_none() {
+            panic!("Pack ID?");
+        }
+        if args.release.is_none() {
+            panic!("Release ID?");
+        }
+        downloader::download(
+            args.pack_id.unwrap(),
+            args.release.unwrap(),
+            args.client.unwrap(),
+            args.out.unwrap(),
+            None,
+        )
+        .await?;
+    } else {
+        gui::gui_main();
     }
-
-    // Parse the targets (minecraft version, forge version & java version)
-    let targets = json["targets"].clone();
-    parse_targets(targets);
 
     Ok(())
 }
