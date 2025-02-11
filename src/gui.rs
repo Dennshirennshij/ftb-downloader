cd4becbc485a44aec2e25d438145f77c13cb1cd2
Dennshirennshij <111683708+Dennshirennshij@users.noreply.github.com>
2025-02-11 15:19:09 +0100

    updated window title

--- src/gui.rs
+++ src/gui.rs
@@ -39,7 +39,7 @@ impl eframe::App for InputApp {
         }
 
         CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Download a FTB Modpack here instead of the \"unique\" app");
+            ui.heading("Download a FTB Modpack here instead of the app");
             ui.horizontal(|ui| {
                 ui.label("Pack ID: ");
                 if self.updates.is_some() {
